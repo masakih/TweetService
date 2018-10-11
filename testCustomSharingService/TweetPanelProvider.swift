c54b463ef0374a0bdc088fd29293bb87e51d9c0f
masakih <masakih@users.sourceforge.jp>
2018-10-11 22:38:45 +0900

    Blurウインドウの処理を関数化した

--- testCustomSharingService/TweetPanelProvider.swift
+++ testCustomSharingService/TweetPanelProvider.swift
@@ -15,84 +15,83 @@ enum TweetPanelProviderError: Error {
 
 class TweetPanelProvider {
     
-    private var tweetPanel: TweetPanelController?
+    private static let panelTopOffset: CGFloat = 40.0
+    
+    private var tweetPanelController: TweetPanelController?
     
     func showTweetPanel(_ sourceWindow: NSWindow?, shareItems items: [Any], completionHandler: @escaping ([Any]) -> Void, cancelHandler: @escaping () -> Void) {
         
-        tweetPanel = TweetPanelController()
+        tweetPanelController = TweetPanelController()
         
-        guard let panel = tweetPanel else { return }
+        guard let panelController = tweetPanelController else { return }
         
-        panel.string = items.first(where: { item in item is String }) as? String ?? ""
-        panel.images = items.filter({ item in item is NSImage }) as? [NSImage] ?? []
+        panelController.string = items.first { item in item is String } as? String ?? ""
+        panelController.images = items.filter { item in item is NSImage } as? [NSImage] ?? []
         
-        if let window = sourceWindow, let panelWindow = panel.window {
-            
-            let targetFrame = window.frame
-            
-            let blurWindowController = BlurWindowController()
-            blurWindowController.window?.addChildWindow(window, ordered: .below)
-            blurWindowController.window?.setFrame(targetFrame, display: false)
-            
-            blurWindowController.showWindow(self)
-            
-            var panelFrame = panelWindow.frame
-            
-            panelFrame.origin.x = targetFrame.origin.x + (targetFrame.width - panelFrame.width) / 2
-            panelFrame.origin.y = targetFrame.origin.y + (targetFrame.height - panelFrame.height) / 2 + 40
-            
-            panelWindow.setFrame(panelFrame, display: false)
-            
-            panelWindow.addChildWindow(blurWindowController.window!, ordered: .below)
-        }
-        
-        panel.completionHandler = { tController in
+        panelController.completionHandler = { tController in
             
             completionHandler(tController.images + [tController.string])
             
-            if let window = sourceWindow, let panelWindow = panel.window, let blurWindow = window.parent {
-                
-                blurWindow.removeChildWindow(window)
-                
-                panelWindow.removeChildWindow(blurWindow)
-                blurWindow.close()
-            }
+            self.closeBlurIfNeed(sourceWindow, tweetPanelController: tController)
             
-            self.tweetPanel = nil
+            self.tweetPanelController = nil
         }
-        panel.cancelHandler = { _ in
+        panelController.cancelHandler = { tController in
             
             cancelHandler()
             
-            if let window = sourceWindow, let panelWindow = panel.window, let blurWindow = window.parent {
-                
-                blurWindow.removeChildWindow(window)
-                
-                panelWindow.removeChildWindow(blurWindow)
-                blurWindow.close()
-            }
+            self.closeBlurIfNeed(sourceWindow, tweetPanelController: tController)
             
-            
-            self.tweetPanel = nil
+            self.tweetPanelController = nil
         }
         
-        panel.showWindow(self)
+        showBlurIfNeed(sourceWindow, tweetPanelController: panelController)
+        panelController.showWindow(self)
     }
     
     func showTweetPanelFuture(_ sourceWindow: NSWindow?, shareItems items: [Any]) -> Future<[Any]> {
         
         let promise = Promise<[Any]>()
         
-        showTweetPanel(sourceWindow, shareItems: items, completionHandler: { items in
-            
-            promise.success(items)
-            
-        }, cancelHandler: {
-            
-            promise.failure(TweetPanelProviderError.userCancel)
-        })
+        showTweetPanel(sourceWindow,
+                       shareItems: items,
+                       completionHandler: { items in promise.success(items) },
+                       cancelHandler: { promise.failure(TweetPanelProviderError.userCancel) })
         
         return promise.future
     }
     
+    private func showBlurIfNeed(_ window: NSWindow?, tweetPanelController: TweetPanelController) {
+        
+        if let window = window, let panelWindow = tweetPanelController.window {
+            
+            let targetFrame = window.frame
+            
+            let blurWindowController = BlurWindowController()
+            blurWindowController.window?.addChildWindow(window, ordered: .below)
+            blurWindowController.window?.setFrame(targetFrame, display: false)
+            blurWindowController.showWindow(self)
+            
+            var panelFrame = panelWindow.frame
+            
+            panelFrame.origin.x = targetFrame.origin.x + (targetFrame.width - panelFrame.width) / 2
+            panelFrame.origin.y = targetFrame.origin.y + (targetFrame.height - panelFrame.height) / 2 + type(of: self).panelTopOffset
+            
+            panelWindow.setFrame(panelFrame, display: false)
+            panelWindow.addChildWindow(blurWindowController.window!, ordered: .below)
+        }
+    }
+    
+    private func closeBlurIfNeed(_ window: NSWindow?, tweetPanelController: TweetPanelController) {
+        
+        if let window = window,
+            let panelWindow = tweetPanelController.window,
+            let blurWindow = window.parent {
+            
+            blurWindow.removeChildWindow(window)
+            
+            panelWindow.removeChildWindow(blurWindow)
+            blurWindow.close()
+        }
+    }
 }
